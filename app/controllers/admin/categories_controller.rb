2e0759cc0893bbe386e413fc47e506c10794d868
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-11-13 08:14:13 +0000

    implemented admin/categories#index

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,7 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+  def index
+    @categories = Category.all
+    # @products_count = Product.where('category_id': @categories).count
+  end
+end
