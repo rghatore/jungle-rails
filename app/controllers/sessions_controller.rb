07003df5c799b5476e6e2b60f390bcaaadffa878
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-11-14 06:05:33 +0000

    added user registration/login/logout feature

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    puts "login email: #{params[:email]}"
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to :root
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    puts "where is the error? #{@user}"
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
